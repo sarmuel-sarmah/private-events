b94f322aab6fd7e46d59a649114ed3eb6554e42f
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-22 17:01:47 +0300

    Add creator id to events table and migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_201855) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_140026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_12_201855) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
