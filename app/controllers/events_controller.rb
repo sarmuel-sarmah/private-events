11679f8f18cac1bd9fe8ccc4f08b798ebdd4d8ed
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-22 18:01:03 +0300

    fix: undefined events method causing error replaced with created_events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,15 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :authenticate_user!, except: [ :index ]
   def index
     @event = Event.all
   end
 
   def new
-    @event = current_user.events.build
+    @event = current_user.created_events.build
   end
 
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.created_events.build(event_params)
 
     if @event.save
       redirect_to @event, notice: "Event was succcessfully created."
