b4e6a6579c1daa22166484b1cba212c9c5e37288
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-22 03:57:13 +0300

    Override the routes generated for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   resources :events
   get "up" => "rails/health#show", as: :rails_health_check
 
