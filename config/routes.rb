a22ca06704e63e74f66ab30672bad2dcb5f23202
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-22 16:33:15 +0300

    Remove restriction on devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: "users/sessions"
-  }
+  devise_for :users
   resources :events
   get "up" => "rails/health#show", as: :rails_health_check
 
