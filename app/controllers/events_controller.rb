479f50d47a4874b5a606cd3d08070d8b6fd0ad9c
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-22 17:40:48 +0300

    Update events controller to allow a user to create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,25 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :index, :show ]
   def index
     @event = Event.all
   end
 
   def new
-    @event = current_user.events.new
+    @event = current_user.events.build
   end
 
   def create
-    @event = current_user.events.build(params[:event])
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event was succcessfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.expect(:title, :description, :date)
   end
 end
