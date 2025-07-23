5767d9732f2e7a5d3bc687ba76530ba6730e9e42
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-23 23:58:05 +0300

    feat: add association to record number of attendees at event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :event_attendings, foreign_key: :event_attendee_id
+  has_many :attendees, through: :event_attendings, source: :event_attendee
   validates :title, :desc, :date, :location, presence: true
 end
