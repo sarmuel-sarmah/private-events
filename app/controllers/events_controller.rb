05b68ed68d608d9f86ab864c69ba1181fb31829b
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-23 20:46:14 +0300

    fix: index action to use events variable instead of event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
   def index
-    @event = Event.all.order(created_at: :desc)
+    @events = Event.all.order(created_at: :desc)
   end
 
   def new
