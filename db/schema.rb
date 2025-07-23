8b5d466ecb331c349840024cdf1067f15274b26c
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-24 00:00:25 +0300

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_23_185011) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_23_204650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "event_attendings", force: :cascade do |t|
+    t.integer "event_attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
