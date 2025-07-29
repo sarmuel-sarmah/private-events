8e6bbbb6fc4fc0e26ac75dc4a558ad8f5f2da2bb
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-29 23:44:36 +0300

    Add foreign key constaint to user id column

--- db/migrate/20250722140026_add_creator_id_to_events.rb
+++ db/migrate/20250722140026_add_creator_id_to_events.rb
@@ -1,5 +1,6 @@
 class AddCreatorIdToEvents < ActiveRecord::Migration[8.0]
   def change
     add_column :events, :creator_id, :integer
+    add_foreign_key :event, :users, column: :creator_id
   end
 end
