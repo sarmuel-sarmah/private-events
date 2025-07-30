777252e1b316374798552ebe0f018d172ef15e25
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-07-30 15:02:05 +0300

    Seperate past and upcoming events on events index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,14 @@ class Event < ApplicationRecord
   has_many :event_attendings, foreign_key: :event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
   validates :title, :desc, :date, :location, presence: true
+
+  class << self
+    def upcoming
+      where("date > ?", Time.current)
+    end
+
+    def past
+      where("date < ?", Time.current)
+    end
+  end
 end
